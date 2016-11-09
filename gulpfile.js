120a627afdfcfef91402100b39d25925d5850386
enepac <enrico.acha@gmail.com>
2016-11-09 21:48:06 +0300

    modified .gitignore, added:es6/ public/es6 with gulpfile.js and package.json along with it.

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+var gulp = require('gulp');
+var babel = require('gulp-babel');
+
+gulp.task('default', function() {
+    // Node source
+    gulp.src("es6/**/*.js")
+    .pipe(babel())
+    .pipe(gulp.dest("dist"));
+    // browser source
+    gulp.src("public/es6/**/*.js")
+    .pipe(babel())
+    .pipe(gulp.dest("public/dist"));
+});
